c440e2912f2b093e84834642b908d090d7675a02
Bismark Osei <bismarkoseiabena@gmail.com>
2022-06-14 20:05:59 +0000

    Update App.jsx
    made changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia.js";
 
-function insertDict(emojipedia => {
-  return (
-    <Entry
-      key={emojipedia.id}
-      emoji={emojipedia.emoji}
-      name={emojipedia.name}
-      meaning={emojipedia.meaning}
-    />
-  );
-});
-
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(insertDict)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojipedia) => (
+          <Entry
+            key={emojipedia.id}
+            emoji={emojipedia.emoji}
+            name={emojipedia.name}
+            meaning={emojipedia.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
